ca377cdc8af2b42d24b83db46c0eb44e4bf054a4
cgint <christian.gintenreiter@gmail.com>
2024-03-30 06:20:20 +0100

    Add some logs as there is an error running this on cloudflare that i did not get locally

--- src/routes/api/searchdetail/search_detail_endpoint.ts
+++ src/routes/api/searchdetail/search_detail_endpoint.ts
@@ -19,17 +19,22 @@ export class BraveSearchDetailEndpoint {
         if (!query) {
             return json({ error: 'Query parameter is required' }, { status: 400 });
         }
-    
+        let stage = 0;
         try {
             console.log("searching", query);
+            stage=1;
             const data = await this.braveSearchDetailService.fetchDetails(query);
             console.log("about to call toDocuments");
+            stage=2;
             const docs = this.braveSearchDetailService.toDocuments(data);
             console.log("about to call QueryVector");
+            stage=3;
             const v = new QueryVector();
             console.log("about to call query on vector");
+            stage=4;
             const result = await v.query(query, docs);
             console.log("about to return result");
+            stage=5;
             return json({ vector: result, plain: data });
         } catch (err) {
             console.error("search error", err);
