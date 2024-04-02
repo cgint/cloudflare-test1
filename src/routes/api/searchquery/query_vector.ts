de2e9b3bea445d0d30c310ab882801887cfcde91
cgint <christian.gintenreiter@gmail.com>
2024-04-02 21:29:13 +0200

    add support for groq and cloudflare-workerai

--- src/routes/api/searchquery/query_vector.ts
+++ src/routes/api/searchquery/query_vector.ts
@@ -8,11 +8,16 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { TogetherAIEmbeddings } from "@langchain/community/embeddings/togetherai";
 import { TogetherAI } from "@langchain/community/llms/togetherai";
+import { ChatGroq } from "@langchain/groq";
+import { ChatCloudflareWorkersAI } from "@langchain/cloudflare";
 import type { MySearchResult } from "../search/brave_search";
 import type { MyDetailSearchResult, SearchEngineResult } from "../searchdetail/brave_search_detail";
 
 const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 const TOGETHER_AI_API_KEY = import.meta.env.VITE_TOGETHER_AI_API_KEY;
+const GROQ_API_KEY = import.meta.env.VITE_GROQ_API_KEY;
+const CLOUDFLARE_ACCOUNT_ID = import.meta.env.VITE_CLOUDFLARE_ACCOUNT_ID;
+const CLOUDFLARE_API_TOKEN = import.meta.env.VITE_CLOUDFLARE_API_TOKEN;
 
 function getEmbeddingsProviderFor(model: string) {
     if (model.startsWith("togethercomputer/")) {
@@ -31,7 +36,19 @@ function getEmbeddingsProviderFor(model: string) {
 }
 
 function getLlmFor(model: string) {
-    if (model.startsWith("togethercomputer/")) {
+    if (model.startsWith("cloudflare/")) {
+        return new ChatCloudflareWorkersAI({
+            model: model.replace("cloudflare/", ""),
+            cloudflareAccountId: CLOUDFLARE_ACCOUNT_ID,
+            cloudflareApiToken: CLOUDFLARE_API_TOKEN,
+          });
+    } else if (model.startsWith("groq/")) {
+        return new ChatGroq({
+            temperature: 0.1,
+            apiKey: GROQ_API_KEY,
+            modelName: model.replace("groq/", "")
+          });
+    } else if (model.startsWith("togethercomputer/")) {
         return new TogetherAI({
             temperature: 0.1, maxTokens: 100,
             apiKey: TOGETHER_AI_API_KEY,
