cc5722aa79bc96ebed7fe808571b7ba448a1c07d
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2025-01-06 19:27:10 +0100

    remove unused models, fix wrangler config

--- src/lib/libraries/llm.ts
+++ src/lib/libraries/llm.ts
@@ -1,20 +1,9 @@
-import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
-import { TogetherAIEmbeddings } from "@langchain/community/embeddings/togetherai";
-import { TogetherAI } from "@langchain/community/llms/togetherai";
-import { ChatGroq } from "@langchain/groq";
-// import { ChatCloudflareWorkersAI } from "@langchain/cloudflare";
 import { ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 import { BaseChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
 
-
-const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
-const TOGETHER_AI_API_KEY = import.meta.env.VITE_TOGETHER_AI_API_KEY;
-const GROQ_API_KEY = import.meta.env.VITE_GROQ_API_KEY;
-// const CLOUDFLARE_ACCOUNT_ID = import.meta.env.VITE_CLOUDFLARE_ACCOUNT_ID;
-// const CLOUDFLARE_API_TOKEN = import.meta.env.VITE_CLOUDFLARE_API_TOKEN;
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
 
@@ -25,17 +14,7 @@ export async function invokeChain(prompt: BaseChatPromptTemplate, input: any) {
 }
 
 export function getEmbeddingsProviderFor(model: string) {
-    if (model.startsWith("togethercomputer/")) {
-        return new TogetherAIEmbeddings({
-            apiKey: TOGETHER_AI_API_KEY,
-            modelName: model
-        });
-    } else if (model.startsWith("openai/")) {
-        return new OpenAIEmbeddings({
-            openAIApiKey: OPENAI_API_KEY,
-            modelName: model.replace("openai/", "")
-        });
-    } else if (model.startsWith("google/")) {
+    if (model.startsWith("google/")) {
         return new GoogleGenerativeAIEmbeddings({
             apiKey: GEMINI_API_KEY,
             modelName: model.replace("google/", "")
@@ -48,32 +27,7 @@ export function getEmbeddingsProviderFor(model: string) {
 export function getLlmFromEnvironment(): BaseChatModel {
     let model = import.meta.env.VITE_INFERENCE_MODEL;
     console.log("using model", model);
-    // if (model.startsWith("cloudflare/")) {
-    //     return new ChatCloudflareWorkersAI({
-    //         model: model.replace("cloudflare/", ""),
-    //         cloudflareAccountId: CLOUDFLARE_ACCOUNT_ID,
-    //         cloudflareApiToken: CLOUDFLARE_API_TOKEN,
-    //       });
-    // } else 
-    if (model.startsWith("groq/")) {
-        return new ChatGroq({
-            temperature: 0.1, maxTokens: 1024,
-            apiKey: GROQ_API_KEY,
-            modelName: model.replace("groq/", "")
-          });
-    // } else if (model.startsWith("togethercomputer/")) {
-    //     return new TogetherAI({
-    //         temperature: 0.1, maxTokens: 1024,
-    //         apiKey: TOGETHER_AI_API_KEY,
-    //         modelName: model.replace("togethercomputer/", "")
-    //     });
-    } else if (model.startsWith("openai/")) {
-        return new ChatOpenAI({
-            temperature: 0.1, maxTokens: 1024,
-            openAIApiKey: OPENAI_API_KEY,
-            modelName: model.replace("openai/", "")
-        });
-    } else if (model.startsWith("google/")) {
+    if (model.startsWith("google/")) {
         return new ChatGoogleGenerativeAI({
             temperature: 0.1, maxOutputTokens: 1024,
             apiKey: GEMINI_API_KEY,
